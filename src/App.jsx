3a16a4d05e897a2c05e247f76389e63419f1f64b
unknown <adityakrishna585@gmai.com>
2025-04-16 13:40:44 +0530

    Updated components and fixed layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Login from './pages/Login'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home';
 import About from './pages/About';
-import Contact from './pages/Contact';
+
 import product from './pages/Product';
 import Cart from './pages/Cart';
 import Product from './pages/Product';
