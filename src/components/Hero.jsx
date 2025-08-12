890d62e4c0a7b10679a76fd9fc3beaf827cc14e0
unknown <adityakrishna585@gmai.com>
2025-08-13 01:13:43 +0530

    Updated code after push

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,45 +1,44 @@
 import React from 'react'
 import { heroImg } from '../assets/frontend_assets/assets'
 import { useEffect } from 'react'
-function Hero({scrollToLatestCollection}) {
-  const[imgIndex, setImgIndex] = React.useState(0)
+function Hero({ scrollToLatestCollection }) {
+  const [imgIndex, setImgIndex] = React.useState(0)
   useEffect(() => {
-    const interval =setInterval(() => {
-      setImgIndex(index => {return ((index+1)%heroImg.length)})
-    },4000)
+    const interval = setInterval(() => {
+      setImgIndex(index => { return ((index + 1) % heroImg.length) })
+    }, 4000)
     return () => clearInterval(interval);
-    },[])
+  }, [])
   return (
-    <div className='flex flex-col sm:flex-row
-    border border-gray-400 w-full bg-white
+    <div className='border border-gray-400 w-full bg-white
     '>
-    <section className="w-full min-h-[500px] flex items-center justify-center bg-white px-6">
-  <div className="max-w-7xl w-full grid sm:grid-cols-[3fr_2fr] gap-10 items-center">
-    
-    {/* Text Section */}
-    <div>
-      <h1 className="text-4xl text-gray-500 sm:text-5xl font-bold leading-tight mb-6">
-        Welcome to <span className="text-black">Your Brand</span>
-      </h1>
-      <p className="text-gray-600 text-lg mb-2">
-        We help you build your digital presence with style, power, and purpose.
-      </p>
-       
-      <button onClick={scrollToLatestCollection} className="bg-black text-white px-6 py-3 rounded-xl hover:bg-gray-700 transition-all">
-        Get Started
-      </button>
-    </div>
+      <section className="w-full min-h-[500px]  bg-white px-6 py-6 sm:py-0 ">
+        <div className="max-w-7xl w-full grid grid-cols-1 sm:grid-cols-[3fr_2fr] gap-10 items-center">
 
-    {/* Image Section */}
-    <div className="w-full h-full flex justify-center items-center overflow-hidden">
-      <img 
-        src={heroImg[imgIndex]}
-        alt="Hero" 
-        className="w-full max-w-md h-auto object-contain rounded-xl shadow-lg"
-      />
-    </div>
-  </div>
-</section>
+          {/* Text Section */}
+          <div>
+            <h1 className="text-4xl text-gray-500 sm:text-5xl font-bold leading-tight mb-6">
+              Welcome to <span className="text-black">Your Brand</span>
+            </h1>
+            <p className="text-gray-600 text-lg mb-2">
+              We help you build your digital presence with style, power, and purpose.
+            </p>
+
+            <button onClick={scrollToLatestCollection} className="bg-black text-white px-6 py-3 rounded-xl hover:bg-gray-700 transition-all">
+              Get Started
+            </button>
+          </div>
+
+          {/* Image Section */}
+          <div className="w-full h-full flex justify-center items-center overflow-hidden">
+            <img
+              src={heroImg[imgIndex]}
+              alt="Hero"
+              className="w-full max-w-md h-auto object-contain rounded-xl shadow-lg"
+            />
+          </div>
+        </div>
+      </section>
 
 
     </div>
